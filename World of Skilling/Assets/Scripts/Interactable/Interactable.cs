25e73f6ebff9baea954db8845d361f68575721da
DaveyDB <davey.debruyn@pearlchain.net>
2018-05-04 12:38:47 +0200

    Add interaction radius and actions for interactable objects

--- World of Skilling/Assets/Scripts/Interactable/Interactable.cs
+++ World of Skilling/Assets/Scripts/Interactable/Interactable.cs
@@ -2,11 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour {
+public abstract class Interactable : MonoBehaviour {
+
+    private bool clickable = false;
 
     private void OnMouseOver()
     {
-        Debug.Log("Mouse is over GameObject");
+        //Debug.Log("Mouse is over GameObject");
     }
 
+    private void OnMouseDown()
+    {
+        if(clickable)
+        {
+            Interact();
+        }
+        else
+        {
+            MoveToInteractable();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name=="Player")
+        {
+            clickable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name=="Player")
+        {
+            clickable = false;
+        }
+    }
+
+    private void MoveToInteractable()
+    {
+        Debug.Log("Add code to move to Interactable");
+    }
+
+    protected abstract void Interact();
 }
